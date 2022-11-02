20c6742c332ab6e03075cdc5a01da896a4d2be71
Ocman Nazir Briet <49003689+ocman-nazir-briet@users.noreply.github.com>
2022-11-03 00:53:44 +0500

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -7,5 +7,6 @@ class myUser(models.Model):
     name = models.CharField(max_length=100)
     photo = models.ImageField(upload_to='userPics', default='noPic.png')
 
+    
     def __str__(self):
         return str(self.name)
