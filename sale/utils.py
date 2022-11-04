dab3ca547b8e343a0b3879bb034978d3b5606e03
usmannazirbareet <usmannazirbareet@gmail.com>
2022-11-04 21:33:46 +0500

    updates

--- sale/utils.py
+++ sale/utils.py
@@ -13,7 +13,6 @@ from django.contrib.auth.models import User
 
 def generate_code():
     # code = str(datetime.now()).replace(' ', '-').upper()[:32]
-
     code = str(uuid.uuid4()).replace('-', '').upper()[:12]
     return code
 
