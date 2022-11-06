d1e09a9ea117cbd5ad4a277f14c3b23ae2a9b8ee
Ocman Nazir Briet <49003689+ocman-nazir-briet@users.noreply.github.com>
2022-11-06 23:20:59 +0500

    Update urls.py

--- src/urls.py
+++ src/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sale.urls')),
     
-    path('', include('sale.urls')),
 ]
 urlpatterns+= static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
