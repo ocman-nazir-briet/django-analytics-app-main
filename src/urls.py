2e3e06188942f71b30059670b2e68f5fdbb85c33
Ocman Nazir Briet <49003689+ocman-nazir-briet@users.noreply.github.com>
2022-11-06 23:21:33 +0500

    Update urls.py

--- src/urls.py
+++ src/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-app_name=""
+app_name="xyz"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
